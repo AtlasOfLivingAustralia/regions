6084a18b112c41cd10644052df24ff3b13d7bd92
markew <mark.woolston@axon.com.au>
2011-07-31 23:01:10 +0000

    initial prototype
    git-svn-id: http://ala-regions.googlecode.com/svn/trunk@2 e0101d6a-932c-3db7-c8fc-2ad22087cb86

---
+++ grails-app/controllers/pt/RegionController.groovy
@@ -0,0 +1,20 @@
+package pt
+
+class RegionController {
+
+    static defaultAction = 'regions3'
+
+    /**
+     * Display the top-level regions page.
+     */
+    def regions = {
+        [initialDisplay:'GER']
+    }
+
+    def regions2 = {}
+    
+    def regions3 = {}
+
+    def test = {}
+    def test2 = {}
+}
