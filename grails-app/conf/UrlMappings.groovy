7c1993290e3ee249134d703eddfd73096a99499e
markew <mark.woolston@axon.com.au>
2011-11-23 00:30:51 +0000

    simplify urls
    git-svn-id: http://ala-regions.googlecode.com/svn/trunk@64 e0101d6a-932c-3db7-c8fc-2ad22087cb86

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,19 +2,25 @@ class UrlMappings {
 
 	static mappings = {
 
-        "/$regionType/$regionName?" (controller: 'regions', action: 'region') {
+        "/$regionType/$regionName" (controller: 'regions', action: 'region') {
             constraints {
                 regionType(inList:['states','lgas','ibras','imcras','nrms','layer'])
             }
         }
         
+        "/$regionType" (controller: 'regions', action: 'regions') {
+            constraints {
+                regionType(inList:['states','lgas','ibras','imcras','nrms','layer'])
+            }
+        }
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
 
-		"/"(view:"/index")
+		"/"(controller: 'regions')
 		"500"(view:'/error')
 	}
 }
