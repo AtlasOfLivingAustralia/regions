1d797bf940a4cdc16394613ae1e0388e38672dcd
markew <mark.woolston@axon.com.au>
2012-02-07 00:15:32 +0000

    make breadcrumb trail configurable
    git-svn-id: http://ala-regions.googlecode.com/svn/trunk@92 e0101d6a-932c-3db7-c8fc-2ad22087cb86

--- grails-app/taglib/au/org/ala/regions/RegionsTagLib.groovy
+++ grails-app/taglib/au/org/ala/regions/RegionsTagLib.groovy
@@ -1,5 +1,7 @@
 package au.org.ala.regions
 
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
 class RegionsTagLib {
 
     static namespace = 'rg'
@@ -15,4 +17,24 @@ class RegionsTagLib {
             out << ""
         }
     }
+
+    /**
+     * Write the appropriate breadcrumb trail.
+     *
+     * Checks the config for skin to choose the correct hierarchy.
+     *
+     * @attr home the root of the page - defaults to collections
+     * @attr atBase true if the page is the base page of the root (no link is added)
+     */
+    def breadcrumbTrail = {attrs ->
+        if (ConfigurationHolder.config.ala.skin == 'ala') {
+            out << "<li><a href='${ConfigurationHolder.config.ala.baseURL}'>Home</a></li>" +
+                "<li> <a href='${ConfigurationHolder.config.ala.baseURL}/explore/'>Explore</a></li>"
+        }
+        else {
+            out << "<li><a href='${ConfigurationHolder.config.ala.baseURL}'>Home</a></li>" +
+                "<li> <a href='${ConfigurationHolder.config.ala.baseURL}/species-by-location/'>Locations</a></li>"
+        }
+    }
+
 }
