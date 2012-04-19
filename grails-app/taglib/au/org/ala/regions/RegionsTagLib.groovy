cbaadb386db1a9b741bfd9959f935ec44c963e20
markew <mark.woolston@axon.com.au>
2012-04-19 05:42:34 +0000

    use common tablib for login/out
    git-svn-id: http://ala-regions.googlecode.com/svn/trunk@122 e0101d6a-932c-3db7-c8fc-2ad22087cb86

--- grails-app/taglib/au/org/ala/regions/RegionsTagLib.groovy
+++ grails-app/taglib/au/org/ala/regions/RegionsTagLib.groovy
@@ -1,20 +1,49 @@
 package au.org.ala.regions
 
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
+import au.org.ala.cas.util.AuthenticationCookieUtils
 
 class RegionsTagLib {
 
     static namespace = 'rg'
 
+    /**
+     * Returns the username of the user if logged in else blank
+     *
+     * @attrs noCookie if true the helper cookie will not be used, ie the user must actually be logged in
+     */
     def loggedInUsername = { attrs ->
+        def useCookie = attrs.noCookie != "true"
         if (request.getUserPrincipal()) {
         	out << request.getUserPrincipal().name
         }
-        else if (AuthenticationCookieUtils.cookieExists(request, AuthenticationCookieUtils.ALA_AUTH_COOKIE)) {
+        else if (useCookie && AuthenticationCookieUtils.cookieExists(request, AuthenticationCookieUtils.ALA_AUTH_COOKIE)) {
             out << AuthenticationCookieUtils.getUserName(request)
         }
         else {
             out << ""
+        }
+    }
+
+    /**
+     * Generate the login link for the banner.
+     *
+     * Will be to log in or out based on current auth status.
+     *
+     * @attr fixedAppUrl if supplied will be used for logout instead of the current page
+     *
+     * @deprecated use HeaderFooterTagLib
+     */
+    def loginoutLink2011 = { attrs ->
+        def requestUri = ConfigurationHolder.config.security.cas.serverName + request.forwardURI
+        if (AuthenticationCookieUtils.cookieExists(request, AuthenticationCookieUtils.ALA_AUTH_COOKIE)) {
+            // currently logged in
+            out << link(controller: 'regions', action: 'logout',
+                    params: [casUrl: ConfigurationHolder.config.security.cas.logoutUrl,
+                            appUrl: attrs.fixedAppUrl ?: requestUri]) {'Logout'}
+        } else {
+            // currently logged out
+            out << "<a href='https://auth.ala.org.au/cas/login?service=${requestUri}'><span>Log in</span></a>"
         }
     }
 
