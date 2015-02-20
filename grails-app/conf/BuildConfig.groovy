38b286b2de1d47799c158580d25e4f798788a5f7
Angel Ruiz <aruizca@gmail.com>
2015-02-20 15:14:57 +1100

    Compilation level downgraded to 1.6 from 1.7 so it can be deployed to PROD Servlet spec set to 2.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,10 +1,11 @@
 import grails.util.Environment
 
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.target.level = 1.7
-grails.project.source.level = 1.7
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
 grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.groupId = "au.org.ala"
 
