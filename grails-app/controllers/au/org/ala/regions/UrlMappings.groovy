90d300e0dcf184e3a695df501757fce7c66452b6
nickdos <nickdos@gmail.com>
2017-08-25 17:47:52 +1000

    Added alaAdminPlugin

--- grails-app/controllers/au/org/ala/regions/UrlMappings.groovy
+++ grails-app/controllers/au/org/ala/regions/UrlMappings.groovy
@@ -5,34 +5,29 @@ class UrlMappings {
     static mappings = {
 
         "/logout/logout"(controller: "logout", action: 'logout')
-
         "/feature/$pid"(controller: 'regions', action: 'region')
 
         "/habitats"(controller: 'habitat', action: 'index')
         "/habitats/"(controller: 'habitat', action: 'index')
-
         "/habitat/?$pid"(controller: 'regions', action: 'habitat')
-
         "/habitats/sld"(controller: 'habitat', action: 'sld')
-
         "/habitats/records/$habitatID"(controller: 'habitat', action: 'viewRecords')
 
         name regionByFeature: "/feature/$pid"(controller: 'regions', action: 'region')
-
         name habitatByFeature: "/habitat?/$pid"(controller: 'regions', action: 'habitat')
         name habitatByFeature: "/habitats?/$pid"(controller: 'regions', action: 'habitat')
 
         "/$regionType/$regionName"(controller: 'regions', action: 'region') {
             constraints {
                 //do not match controllers
-                regionType(matches: '(?!(^data\$|^proxy\$|^region\$|^regions\$)).*')
+                regionType(matches: '(?!(^data\$|^proxy\$|^region\$|^regions\$|^alaAdmin\$)).*')
             }
         }
 
         "/$regionType"(controller: 'regions', action: 'regions') {
             constraints {
                 //do not match controllers
-                regionType(matches: '(?!(^data\$|^proxy\$|^region\$|^regions\$)).*')
+                regionType(matches: '(?!(^data\$|^proxy\$|^region\$|^regions\$|^alaAdmin\\$)).*')
             }
         }
 
