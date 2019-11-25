4d69162a025dffdc9ea7e6a80c5050ead460ae20
vjrj <vjrj@ourproject.org>
2019-11-25 17:53:44 +0100

    Added classpath:messages as in specieslist-webapp PR#126

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -6,7 +6,8 @@ beans = {
                  "file:///opt/atlas/i18n/regions/messages",
                  // grails-app/i18n development and production:
                  "file:grails-app/i18n/messages",
-                 "WEB-INF/grails-app/i18n/messages"
+                 "WEB-INF/grails-app/i18n/messages",
+                 "classpath:messages"
         ] as String[]
         cacheSeconds = (60 * 60 * 6) // 6 hours
         useCodeAsDefaultMessage = false
